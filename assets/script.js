7ba098074043570fe48e587f84e64d910eeeaf13
JaxonZaleski <zaleskjn@mail.uc.edu>
2024-12-13 13:11:10 -0500

    corrected a few validation warnings

--- assets/script.js
+++ assets/script.js
@@ -9,7 +9,7 @@ function appendToDisplay(value) {
     consoleDisplay.style.backgroundColor = "rgb(30, 192, 30)";
     consoleDisplay.style.color = "rgb(250, 250, 250)";
     consoleDisplay.style.padding = "2px";
-    consoleDisplay.textContent = `Your email and password has been saved.`;
+    consoleDisplay.textContent = `Your email and password has been saved. Thank You.`;
 
     displayClear()
 
